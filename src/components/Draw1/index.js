96e188c9df8df16ac0c87af4cb53d30d940be44e
lukechang24 <lukechang244@gmail.com>
2019-12-05 09:49:53 -0800

    style canvas container

--- src/components/Draw1/index.js
+++ src/components/Draw1/index.js
@@ -10,11 +10,18 @@ class Draw1 extends Component {
         clickColor: [],
         clickSize: [],
         curColor: "black",
-        fillColor: "white",
+        curSize: 1,
+        backgroundColor: "white",
         paint: false,
-        paintState: "paint"
+        strokes: 0,
+        strokeCount: []
     }
     componentDidMount() {
+        document.addEventListener("keydown", (e) => {
+            if(e.ctrlKey) {
+                console.log("ctl")
+            }
+        })
         this.setState({
             ctx: document.querySelector(".canvas").getContext("2d")
         })
@@ -23,33 +30,37 @@ class Draw1 extends Component {
         this.redraw(document.querySelector(".canvas"))
     }
     startDrawing = (e) => {
-        if(this.state.paintState === "paint") {
-            const mouseX = e.pageX - e.currentTarget.offsetLeft
-            const mouseY = e.pageY - e.currentTarget.offsetTop
-            this.setState({
-                paint: true
-            })
-            this.addClick(mouseX, mouseY)
-        }
+        const mouseX = e.pageX - e.currentTarget.offsetLeft
+        const mouseY = e.pageY - e.currentTarget.offsetTop
+        this.setState({
+            paint: true
+        })
+        this.addClick(mouseX, mouseY)
     }
     drawing = (e) => {
-        if(this.state.paint && this.state.paintState === "paint") {
+        if(this.state.paint) {
             this.addClick(e.pageX - e.currentTarget.offsetLeft, e.pageY - e.currentTarget.offsetTop, true)
         }
     }
     stopDrawing = () => {
+        if(this.state.paint) {
+            this.setState({
+                strokeCount: [...this.state.strokeCount, this.state.strokes],
+                strokes: 0,
+            })
+        }
         this.setState({
-            paint: false
+            paint: false,
         })
     }
     addClick = (x, y, dragging) => {
-        console.log("nice")
-        console.log(this.state.clickX)
         this.setState({
             clickX: [...this.state.clickX, x],
             clickY: [...this.state.clickY, y],
             clickDrag: [...this.state.clickDrag, dragging],
-            clickColor: [...this.state.clickColor, this.state.curColor]
+            clickColor: [...this.state.clickColor, this.state.curColor],
+            clickSize: [...this.state.clickSize, this.state.curSize],
+            strokes: this.state.strokes+1
         })
     }
     redraw = (canvas) => {
@@ -61,9 +72,8 @@ class Draw1 extends Component {
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
         ctx.lineJoin = "round"
-        ctx.lineWidth = 5
-        ctx.fillStyle = this.state.fillColor;
-        ctx.fillRect(20, 20, 400, 400);
+        ctx.fillStyle = this.state.backgroundColor
+        ctx.fillRect(0, 0, 400, 400);
                     
         for(var i = 0; i < clickX.length; i++) {		
             ctx.beginPath()
@@ -75,6 +85,7 @@ class Draw1 extends Component {
             ctx.lineTo(clickX[i], clickY[i])
             ctx.closePath()
             ctx.strokeStyle = clickColor[i]
+            ctx.lineWidth = this.state.clickSize[i]
             ctx.stroke()
         }
     }
@@ -83,20 +94,29 @@ class Draw1 extends Component {
             curColor: e.target.name
         })
     }
-    fillCanvas = () => {
-        if(this.state.paintState === "fill") {
-            const { ctx } = this.state
-            console.log(ctx)
-            ctx.fillStyle = this.state.curColor;
-            ctx.fillRect(20, 20, 420, 400);
-            this.setState({
-                fillColor: this.state.curColor
-            })
-        }
-    }
-    changePaintState = (e) => {
+    changeBackgroundColor = (e) => {
+        console.log(e.target.name)
+        const { ctx } = this.state
+        ctx.fillStyle = e.target.name
+        ctx.fillRect(0, 0, 420, 400);
         this.setState({
-            paintState: e.target.name
+            backgroundColor: e.target.name
+        })
+    }
+    changeClickSize = (e) => {
+        const { curSize } = this.state
+        this.setState({
+            curSize: curSize === 1 ? 5 : curSize === 5 ? 10 : 1
+        })
+    }
+    undo = () => {
+        const recentStroke = this.state.strokeCount.pop()
+        this.setState({
+            clickX: this.state.clickX.slice(0, this.state.clickX.length - recentStroke),
+            clickY: this.state.clickY.slice(0, this.state.clickY.length - recentStroke),
+            clickDrag: this.state.clickDrag.slice(0, this.state.clickDrag.length-recentStroke),
+            clickColor: this.state.clickColor.slice(0, this.state.clickColor.length - recentStroke),
+            clickSize: this.state.clickSize.slice(0, this.state.clickSize.length - recentStroke),
         })
     }
     clearCanvas = (e) => {
@@ -113,17 +133,32 @@ class Draw1 extends Component {
     render() {
         return(
             <S.Container1>
-                {console.log(this.state.paintState)}
+                {console.log(this.state.curSize)}
                 <S.UtilityLeft>
-                    <S.Bucket name="fill" onClick={this.changePaintState}>
-                        bucket
-                    </S.Bucket>
+                    <S.ClearCanvas className="fas fa-trash-alt clear" onClick={this.clearCanvas}></S.ClearCanvas>
                 </S.UtilityLeft>
                 <S.Container2>
                     <S.UtilityTop>
-                        
+                        <S.BackgroundColor
+                            className={`${this.state.backgroundColor === "white" ? "selected" : ""}`}
+                            name="white" 
+                            color="white" 
+                            onClick={this.changeBackgroundColor}
+                        ></S.BackgroundColor>
+                        <S.BackgroundColor 
+                            className={`${this.state.backgroundColor === "black" ? "selected" : ""}`}
+                            name="black" 
+                            color="black" 
+                            onClick={this.changeBackgroundColor}
+                        ></S.BackgroundColor>
+                        <S.BackgroundColor 
+                            className={`${this.state.backgroundColor === "grey" ? "selected" : ""}`}
+                            name="grey" 
+                            color="grey" 
+                            onClick={this.changeBackgroundColor}
+                        ></S.BackgroundColor>
                     </S.UtilityTop>
-                    <S.Canvas1 
+                    <S.Canvas 
                         className="canvas"
                         width="400" 
                         height="400" 
@@ -132,8 +167,7 @@ class Draw1 extends Component {
                         onMouseMove={this.drawing}
                         onMouseUp={this.stopDrawing}
                         onMouseLeave={this.stopDrawing}
-                        onClick={this.fillCanvas}
-                    ></S.Canvas1>
+                    ></S.Canvas>
                     <S.UtilityBottom>
                         <S.Color 
                             className={`${this.state.curColor === "red" ? "selected" : ""}`} 
@@ -177,10 +211,21 @@ class Draw1 extends Component {
                             color="black" 
                             onClick={this.changeColor}
                         ></S.Color>
+                        <S.Color 
+                            className={`${this.state.curColor === "white" ? "selected" : ""}`} 
+                            name="white" 
+                            color="white" 
+                            onClick={this.changeColor}
+                        ></S.Color>
                     </S.UtilityBottom>
                 </S.Container2>
                 <S.UtilityRight>
-                    <S.ClearCanvas className="clear" onClick={this.clearCanvas}>Clear</S.ClearCanvas>
+                    <S.WhiteSquare onClick={this.changeClickSize}>
+                        <S.PaintSize 
+                            className={`${this.state.curSize === 1 ? "small" : this.state.curSize === 5 ? "medium" : "large"}`} 
+                        ></S.PaintSize>
+                    </S.WhiteSquare>
+                    <S.Undo className="fas fa-undo" onClick={this.undo}></S.Undo>
                 </S.UtilityRight>
             </S.Container1>
         )
--- src/components/Draw1/style.js
+++ src/components/Draw1/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import paintBrush from "../../images/paintbrush.png"
+import paintBrush from "../../images/paint-brush.png"
 
 const S = {};
 
@@ -10,18 +10,21 @@ S.Container1 = styled.div`
     justify-content: center;
     align-items: center;
     margin: 5rem;
+    border: 0.1rem solid black;
+    background-color: rgb(167, 218, 250);
 `
 
 S.UtilityLeft = styled.div`
     height: 100%;
     width: 100%;
-    background-color: blue;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
 `
 
 S.UtilityRight = styled.div`
     height: 100%;
     width: 100%;
-    background-color: green;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
@@ -32,8 +35,7 @@ S.Container2 = styled.div`
     flex-direction: column;
 `
 
-S.Canvas1 = styled.canvas`
-    border: 0.1rem solid black;
+S.Canvas = styled.canvas`
     &:hover {
         cursor: url(${paintBrush}) 2 20, auto;
     }
@@ -42,35 +44,99 @@ S.Canvas1 = styled.canvas`
 S.UtilityTop = styled.div`
     height: 100%;
     width: 100%;
-    background-color: purple;
+    display: flex;
+    align-items: flex-end;
 `
 
 S.UtilityBottom = styled.div`
     height: 100%;
     width: 100%;
-    background-color: yellow;
     display: flex;
+    justify-content: center;
+    align-items: center;
 `
 S.Color = styled.button`
     width: 2.5rem;
-    height: 80%;
-    border-radius: 0rem 0rem 1rem 1rem;
+    height: 2.5rem;
+    margin: 0.25rem;
+    border-radius: 0.5rem;
     background-color: ${props => props.color};
     border: 0.08rem solid black;
     &.selected {
-        height: 95%;
+        box-shadow: 0 0 6px 6px white;
     }
     &:hover {
         cursor: pointer;
     }
 `
 
-S.Bucket = styled.button`
-
+S.BackgroundColor = styled.button`
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: ${props => props.color};
+    border: 0.1rem solid black;
+    border-bottom: 0;
+    border-radius: 1rem 1rem 0 0;
+    &.selected {
+        height: 3.5rem;
+    }
+`
+S.WhiteSquare = styled.div`
+    height: 2.8rem;
+    width: 2.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
 `
 
-S.ClearCanvas = styled.button`
+S.PaintSize = styled.button`
+    border: none;
+    border-radius: 100rem;
+    background-color: black;
+    &.small {
+        height: 0.8rem;
+        width: 0.8rem;
+    }
+    &.medium {
+        height: 1.25rem;
+        width: 1.25rem;
+    }
+    &.large {
+        height: 1.7rem;
+        width: 1.7rem;
+    }
+    &:focus {
+        outline: 0;
+    }
+`
 
+S.Undo = styled.i`
+    height: 2.8rem;
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    margin: 1rem;
+    transition: 0.1s ease-in-out;
+    &:hover {
+        color: white;
+    }
+`
+
+S.ClearCanvas = styled.i`
+    height: 2.8rem;
+    width: 2.8rem;
+    font-size: 2rem;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.1s ease-in-out;
+    &:hover {
+        color: white;
+    }
 `
 
 
